646aa776bc69a8790442ca053d99812cee0ee975
Marina Radisavljevic <marina@MacBook-Pro.local>
2019-01-19 20:40:01 +0100

    Removed unneeded variables.

--- src/utils/API.js
+++ src/utils/API.js
@@ -2,7 +2,6 @@
 
 const host = `https://api.staging.brokercloud.io/v1`;
 const client_id = `844b0a54-c0af-11e7-abc4-cec278b6b50a`;
-const applicationId = '345070b3-722f-4fd8-8940-5286c15b53df';
 
 
 export const signIn = (email, password) => {
